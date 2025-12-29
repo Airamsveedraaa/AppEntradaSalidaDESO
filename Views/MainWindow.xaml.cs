3e9a5e2770c4f39656848cb871ffe9e9e5474994
Airamsveedraaa <user@example.com>
2025-12-29 20:05:41 +0000

    refactor: reorganize project structure and cleanup

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace AppEntradaSalidaDESO;
+namespace AppEntradaSalidaDESO.Views;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
